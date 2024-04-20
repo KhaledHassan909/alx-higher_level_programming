7424a9442a8569fc20c57d5956764b216e6434fb
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2024-04-20 11:49:02 +0200

    Task8 added

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for i in s:
+        dec_value = ord(i) #decimal value
+        if(dec_value) >= 97 and dec_value <= 122: #check if current char is lowercase
+            dec_value -= 32 #convert lowercase to uppercase
+            print(chr(dec_value), end = '') #print uppercase
+        else:
+            print(chr(dec_value), end = '') #print uppercase
+    print('\n') #print newline
