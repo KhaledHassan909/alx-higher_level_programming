9ba83787bf977e5199a1ecae42295c5eab58b40b
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2024-04-20 14:51:26 +0200

    Task8 modified

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
-def uppercase(s):
-    for i in s: #Looping through chars
-        dec_value = ord(i)  # decimal value of current char
-        if dec_value >= 97 and dec_value <= 122:  # check if current char is lowercase
-            i = chr(dec_value - 32)  # convert lowercase to uppercase
-        print("{}".format(i), end = "")  # print uppercase
-    print("{}".format(''), end = '\n')
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print()
+
+# def uppercase(s):
+#     for i in s: #Looping through chars
+#         dec_value = ord(i)  # decimal value of current char
+#         if dec_value >= 97 and dec_value <= 122:  # check if current char is lowercase
+#             i = chr(dec_value - 32)  # convert lowercase to uppercase
+#         print("{}".format(i), end = "")  # print uppercase
+#     print("{}".format(''), end = '\n')
